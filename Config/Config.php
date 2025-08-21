14d3934c1adc7d7329a3310d01eacaf5f428c84e
Seocum <siok1joel@gmail.com>
2025-08-20 20:08:35 -0500

    esto lo pones en la ruta C:\xampp\htdocs\ wawa

---
+++ Config/Config.php
@@ -0,0 +1,11 @@
+<?php
+const BASE_URL = "http://localhost/Omedic/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "Omedic";
+const CHARSET = "charset=utf8";
+const TITLE = "Omedic";
+const MONEDA = "PEN";
+const CLIENT_ID = "";
+?>
